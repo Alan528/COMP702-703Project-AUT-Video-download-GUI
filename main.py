212af46e5c0bbf07eb839709e14e11b8a9f49c2e
Alan528 <83804199+Alan528@users.noreply.github.com>
2022-06-09 21:30:14 +0800

    Bilibili Download

--- main.py
+++ main.py
@@ -1,130 +1,76 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
+import requests
+from lxml import html
+import re
+import json
+import os
 
-# @author: yjp
-# @software: PyCharm
-# @file: main.py
-# @time: 2022-02-08 12:52
 
-from requests_html import HTMLSession
-from lxml import etree
-import random,re,time,json,os,subprocess
-from icecream import ic
-session = HTMLSession()
-USER_AGENT_LIST = [
-    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3451.0 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:57.0) Gecko/20100101 Firefox/57.0',
-    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.2999.0 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.70 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2',
-    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36 OPR/31.0.1889.174',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.1.4322; MS-RTC LM 8; InfoPath.2; Tablet PC 2.0)',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 OPR/55.0.2994.61',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.814.0 Safari/535.1',
-    'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; ja-jp) AppleWebKit/418.9.1 (KHTML, like Gecko) Safari/419.3',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
-    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; Touch; MASMJS)',
-    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1041.0 Safari/535.21',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3451.0 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:57.0) Gecko/20100101 Firefox/57.0',
-    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.2999.0 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.70 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2',
-    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36 OPR/31.0.1889.174',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.1.4322; MS-RTC LM 8; InfoPath.2; Tablet PC 2.0)',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 OPR/55.0.2994.61',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.814.0 Safari/535.1',
-    'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; ja-jp) AppleWebKit/418.9.1 (KHTML, like Gecko) Safari/419.3',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
-    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; Touch; MASMJS)',
-    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1041.0 Safari/535.21',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4093.3 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko; compatible; Swurl) Chrome/77.0.3865.120 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4086.0 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:75.0) Gecko/20100101 Firefox/75.0',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/91.0.146 Chrome/85.0.4183.146 Safari/537.36',
-    'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36 VivoBrowser/8.4.72.0 Chrome/62.0.3202.84',
-    'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.60',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:83.0) Gecko/20100101 Firefox/83.0',
-    'Mozilla/5.0 (X11; CrOS x86_64 13505.63.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:68.0) Gecko/20100101 Firefox/68.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.400',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
-]
+def get_title_json(url):
+    head = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.67'
+    }
+    r = requests.get(url, headers=head)
 
-os_path = os.getcwd() + '/statics/'
-if not os.path.exists(os_path):
-    os.mkdir(os_path)
-class bzVideo(object):
-    def __init__(self):
-        self.bv_id = input("请输入BV号：")
-        self.start_url = f"https://www.bilibili.com/video/{self.bv_id}"
-        self.headers = {
-            'cookie': "_uuid=9AA231F3-7101-BE09-7BD8-81060508B89A75764infoc; buvid3=BB7FFBF9-7220-4054-BD27-9E2B59ED908E34751infoc; rpdid=|(u|JJRRY|Rk0J'uYku~lku|J; LIVE_BUVID=AUTO5016226178557228; video_page_version=v_old_home; fingerprint_s=ff4b2ef3470a91c32417af22e8d5a16b; CURRENT_BLACKGAP=0; buvid4=01D09AFB-B416-4588-6D03-9AD185A2ADBD39820-022012515-gGjJrmXHf7W+R9pSvVQ1AQ%3D%3D; fingerprint3=c4ea0454a67324885dd00baab9e63ede; CURRENT_QUALITY=80; i-wanna-go-back=-1; bp_video_offset_699964101=624161960473894100; buvid_fp_plain=undefined; blackside_state=0; fingerprint=688bf56e4ec1e704c5c0efcf1c80c861; bp_t_offset_666724842=624490027260207500; sid=bly5coe7; buvid_fp=688bf56e4ec1e704c5c0efcf1c80c861; bp_video_offset_666724842=624722140169744400; b_ut=7; CURRENT_FNVAL=4048; b_lsid=F2F7D5F9_17ED7A6282B; innersign=1; PVID=9",
-            'referer': 'https://www.bilibili.com/video/BV1zR4y1j7tb',
-            'user-agent' : random.choice(USER_AGENT_LIST)
-        }
-    def parse_start_url(self):
-        response = session.get(self.start_url,headers=self.headers)
-        self.parse_start_response(response)
-    def parse_start_response(self,response):
-        '''
+    # 通过xpath获取视频的标题
+    tree = html.fromstring(r.text)
+    title = str(tree.xpath('//*[@id="viewbox_report"]/h1/@title')[0])
+    # 除去标题中的奇怪字符，这些字符会影响文件名的保存，产生未知错误
+    s = ['\n', '，', '。', ' ', '—', '”', '？', '“', '（', '）', '、', '|']
+    for i in s:
+        title = title.replace(i, '')
+    print(f'视频标题："{title}"')
 
-        :param response:
-        :return: playinfo baseUrl. type str.
-        '''
-        html_str = response.text
-        vTitle = re.findall('<h1 title="(.*?)"', html_str)[0]
-        playinfo = re.findall("window.__playinfo__=(.*?)</script>",html_str)[0]
-        self.parse_data(vTitle,playinfo)
-    def parse_data(self,vTitle,playinfo):
-        data_json = json.loads(playinfo)
+    # 通过re获取包含视频和音频地址的json字段
+    json_data = re.findall('<script>window.__playinfo__=(.*?)</script>', r.text)[0]
+    json_data = json.loads(json_data)
 
-        video_info = data_json["data"]["dash"]["video"][0]
-        audio_info = data_json["data"]["dash"]["audio"][0]
-        self.download_mp4_and_mp3(vTitle,video_info,audio_info)
-    def download_mp4_and_mp3(self,vTitle,video_info,audio_info):
-        video_url = video_info["baseUrl"]
-        audio_url = audio_info["baseUrl"]
-        headers = {
+    audio_url = json_data['data']['dash']['audio'][0]['backupUrl'][0]
+    print('got audio link')
+    video_url = json_data['data']['dash']['video'][0]['backupUrl'][0]
+    print('got video link')
+    return title, audio_url, video_url
 
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
-        }
-        video_content = session.get(video_url, headers=self.headers).content
-        audio_content = session.get(audio_url, headers=self.headers).content
-        self.save_data(vTitle, video_content, audio_content)
-    def save_data(self,vTitle,video_content, audio_content):
-        '''
-                    保存到本地  vTitle 去掉空格
-                    :param vTitle:
-                    :param video_content:
-                    :param audio_content:
-                    :return: vTitle out.mp4
-        '''
 
-        vTitle = vTitle.replace(" ", '')
-        with open(os_path + vTitle + '.mp4', 'wb') as f:
-            f.write(video_content)
-        with open(os_path + vTitle + '.mp3', 'wb') as f:
-            f.write(audio_content)
+def download(title, audio_url, video_url):
+    # 这里的head加上Referer，避免让网站发现自己是爬虫
+    head = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.67',
+        'Referer': url
+    }
+    print('Start to download audio')
+    r_audio = requests.get(url=audio_url, headers=head)
+    audio_data = r_audio.content
+    with open(title + '.mp3', mode='wb') as f:
+        f.write(audio_data)
+    print('Audio download complete')
 
-        vTitle_0 = vTitle + 'out'
-        cmd = r"ffmpeg -i {}/{}.mp4 -i {}/{}.mp3 -c:v copy -c:a aac -strict experimental {}/{}.mp4".format(os_path,vTitle,os_path,vTitle,os_path,vTitle_0)
-        subprocess.run(cmd, shell=True)
-if __name__ == '__main__':
-    b = bzVideo()
-    b.parse_start_url()
+    print('Start to download vedio')
+    print('Downloading')
+    r_video = requests.get(url=video_url, headers=head)
+    video_data = r_video.content
+    with open(title + '.mp4', mode='wb') as f:
+        f.write(video_data)
+    print('video download complete')
+
+
+def audio_video_add(title):
+    print('Compositing')
+    cmd = f' ffmpeg  -i {title}.mp4 -i {title}.mp3 -acodec copy -vcodec copy bili_{title}.mp4'
+    # 调用cmd执行ffmpeg程序来合并音频和视频
+    os.system(cmd)
+    # 删除原来的音频和视频
+    os.remove(title + '.mp4')
+    os.remove(title + '.mp3')
+    print('Composited')
+
+
+if __name__ == "__main__":
+    while True:
+        value = input('Please input video link quite to exic）')
+        if value == 'quit':
+            break
+        else:
+            url = value
+            title, audio_url, video_url = get_title_json(url)
+            download(title, audio_url, video_url)
+            audio_video_add(title)
