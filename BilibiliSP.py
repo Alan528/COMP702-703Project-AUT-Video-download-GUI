47abbee1c6803de98517fc55e8722d658ab2cee9
Alan528 <83804199+Alan528@users.noreply.github.com>
2022-06-29 01:22:01 +0800

    A

--- BilibiliSP.py
+++ BilibiliSP.py
@@ -6,49 +6,64 @@ import os
 import main
 
 
-def bilibili(url):
+def bilibili(url, userinput):
     headers = {
         'referer': 'https://www.bilibili.com/',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
 
     }
+    try:
+        response = requests.get(url=url, headers=headers)
+        # print(response.text) #获取网页数据
 
-    response = requests.get(url=url, headers=headers)
-    # print(response.text) #获取网页数据
+        title = re.findall('<h1 title="(.*?)"', response.text)[0]
+        s = ['\n', '，', '。', ' ', '—', '”', '？', '“', '（', '）', '、', '|']
+        for i in s:
+            title = title.replace(i, '')
+        print(f'Video Title："{title}"')
+        print("Downloading")
 
-    title = re.findall('<h1 title="(.*?)"', response.text)[0]
-    s = ['\n', '，', '。', ' ', '—', '”', '？', '“', '（', '）', '、', '|']
-    for i in s:
-        title = title.replace(i, '')
-    print(f'Video Title："{title}"')
-    print("Downloading")
+        playinfo = re.findall('<script>window.__playinfo__=(.*?)</script>', response.text)[0]
 
-    playinfo = re.findall('<script>window.__playinfo__=(.*?)</script>', response.text)[0]
+        # print(playinfo)
+        # print(type(playinfo))
 
-    # print(playinfo)
-    # print(type(playinfo))
+        json_playinfo = json.loads(playinfo)  # 转换playinfo数据类型为字典
+        # print(type(json_playinfo))
+        # pprint.pprint(json_playinfo) #格式化输出
 
-    json_playinfo = json.loads(playinfo)  # 转换playinfo数据类型为字典
-    # print(type(json_playinfo))
-    # pprint.pprint(json_playinfo) #格式化输出
+        audio_url = json_playinfo['data']['dash']['audio'][0]['baseUrl']
+        video_url = json_playinfo['data']['dash']['video'][0]['baseUrl']
 
-    audio_url = json_playinfo['data']['dash']['audio'][0]['baseUrl']
-    video_url = json_playinfo['data']['dash']['video'][0]['baseUrl']
+        # print(audio_url)
+        # print(video_url)
 
-    print(audio_url)
-    print(video_url)
+        audio_content = requests.get(url=audio_url, headers=headers).content
+        video_content = requests.get(url=video_url, headers=headers).content
 
-    audio_content = requests.get(url=audio_url, headers=headers).content
-    video_content = requests.get(url=video_url, headers=headers).content
-    with open(title + '.mp3', mode='wb') as f:
-        f.write(audio_content)
-    with open(title + '.mp4', mode='wb') as f:
-        f.write(video_content)
+        if userinput == "1":
+            with open(title + '.mp3', mode='wb') as f:
+                f.write(audio_content)
+            with open(title + '.mp4', mode='wb') as f:
+                f.write(video_content)
 
-    cmd = f' ffmpeg  -i {title}.mp4 -i {title}.mp3 -acodec copy -vcodec copy bili_{title}.mp4'
-    # 调用cmd执行ffmpeg程序来合并音频和视频
-    os.system(cmd)
-    # 删除原来的音频和视频
-    os.remove(title + '.mp4')
-    os.remove(title + '.mp3')
-    print('Download completed')
+            cmd = f' ffmpeg  -i {title}.mp4 -i {title}.mp3 -acodec copy -vcodec copy bili_{title}.mp4'
+            # 调用cmd执行ffmpeg程序来合并音频和视频
+            os.system(cmd)
+            # 删除原来的音频和视频
+            os.remove(title + '.mp4')
+            os.remove(title + '.mp3')
+            print('Download completed')
+
+        elif userinput == "2":
+            with open(title + 'bili_.mp4', mode='wb') as f:
+                f.write(video_content)
+            print('Download completed')
+
+        elif userinput == "3":
+            with open(title + 'bili_.mp3', mode='wb') as f:
+                f.write(audio_content)
+            print('Download completed')
+
+    except:
+        print("Please input a exit Bilibili video link")
--- YoutubeSp.py
+++ YoutubeSp.py
@@ -4,43 +4,45 @@ import re
 import os
 
 
-def youtube(url):
-            headers = {
-                'cookie': 'VISITOR_INFO1_LIVE=En-lfqNNXQw; PREF=tz=Asia.Hong_Kong&f4=4000000&f5=30000; GPS=1; YSC=o1EVGXJ0H7I',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36',
-                'referer': 'https://www.youtube.com'
-            }
+def youtube(url, userinput):
+    headers = {
+        'cookie': 'VISITOR_INFO1_LIVE=En-lfqNNXQw; PREF=tz=Asia.Hong_Kong&f4=4000000&f5=30000; GPS=1; YSC=o1EVGXJ0H7I',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36',
+        'referer': 'https://www.youtube.com'
+    }
+    try:
+        respons = requests.get(url=url, headers=headers)
+        json_str = re.findall('var ytInitialPlayerResponse = (.*?);var', respons.text)[0]
+        # print(json_str)
+        json_data = json.loads(json_str)
 
-            respons = requests.get(url=url, headers=headers)
+        videourl = json_data['streamingData']['adaptiveFormats'][0]['url']
+        audiourl = json_data['streamingData']['adaptiveFormats'][-2]['url']
 
-            json_str = re.findall('var ytInitialPlayerResponse = (.*?);var', respons.text)[0]
-            # print(json_str)
-            json_data = json.loads(json_str)
+        title = json_data['videoDetails']['title']
 
-            videourl = json_data['streamingData']['adaptiveFormats'][0]['url']
-            audiourl = json_data['streamingData']['adaptiveFormats'][-2]['url']
+        # print(videourl)
+        # print(audiourl)
 
-            title = json_data['videoDetails']['title']
+        s = ['\n', '，', '。', ' ', '—', '”', '？', '“', '（', '）', '、', '|']
+        for i in s:
+            title = title.replace(i, '')
+        print(f'Video Title："{title}"')
 
-            print(videourl)
-            print(audiourl)
+        print("Downloading...")
+        video = requests.get(videourl)
+        audio = requests.get(audiourl)
 
-            s = ['\n', '，', '。', ' ', '—', '”', '？', '“', '（', '）', '、', '|']
-            for i in s:
-                title = title.replace(i, '')
-            print(f'Video Title："{title}"')
-
-            print("Downloading...")
-            video = requests.get(videourl)
+        if userinput == "1":
             with open(f'{title}.mp4', mode='wb') as f:
                 f.write(video.content)
 
-            audio = requests.get(audiourl)
+
             with open(f'{title}.mp3', mode='wb') as f:
                 f.write(audio.content)
 
             # 调用cmd执行ffmpeg程序来合并音频和视频
-            cmd = f'ffmpeg -i "{title}.mp4" -i "{title}.mp3" -c:v copy -c:a aac -strict -2 "Youtube{title}.mp4"'
+            cmd = f'ffmpeg -i "{title}.mp4" -i "{title}.mp3" -c:v copy -c:a aac -strict -2 "Youtube_{title}.mp4"'
             os.system(cmd)
 
             # 删除原来的音频和视频
@@ -48,3 +50,16 @@ def youtube(url):
             os.remove(title + '.mp3')
             print('Download completed')
 
+        elif userinput == "2":
+            with open(f'{title}_Youtube.mp4', mode='wb') as f:
+                f.write(video.content)
+            print('Download completed')
+
+        elif userinput == "3":
+            with open(f'{title}_Youtube.mp3', mode='wb') as f:
+                f.write(audio.content)
+            print('Download completed')
+
+
+    except:
+        print("Please input a exit Youtube video link")
