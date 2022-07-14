36bcdc1ae27d36dab7cfdf3cefc464251259c761
Alan528 <83804199+Alan528@users.noreply.github.com>
2022-07-14 23:32:26 +0800

    fix the file will not run if the location change

--- test_ff.py
+++ test_ff.py
@@ -1,16 +1,30 @@
 import os
 import re
 import time
+import sys
 
+if __name__ == '__main__':
 
-def test_ff():
-    path = os.environ['path']
-    if re.findall("ffmpeg", path):
+    s = ['\\']
+    path = str(os.environ['path'])
+    for i in s:
+        path = path.replace(i, '/')
+        print(path)
+
+    current_Path = os.getcwd()
+    for i in s:
+        current_Path = current_Path.replace(i, '/')
+        print(current_Path)
+
+    ffe_part = "/ffmpeg-master-latest-win64-gpl-shared/bin"
+
+    join_part = current_Path + ffe_part
+    print(join_part)
+
+    if re.findall(join_part, path):
         print("ffmpeg exit")
-
     else:
         print("ffmpeg not exit, Installing that will restart the program")
         os.system("Install.bat")
         time.sleep(3)
         exit()
-
