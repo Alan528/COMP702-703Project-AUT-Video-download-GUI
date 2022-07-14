39dd9f31bed47dadbfcfba1358db200d49b99e6b
Alan528 <83804199+Alan528@users.noreply.github.com>
2022-07-14 23:35:58 +0800

    change

--- test_ff.py
+++ test_ff.py
@@ -3,23 +3,23 @@ import re
 import time
 import sys
 
-if __name__ == '__main__':
 
+def test_ff():
     s = ['\\']
     path = str(os.environ['path'])
     for i in s:
         path = path.replace(i, '/')
-        print(path)
+        # print(path)
 
     current_Path = os.getcwd()
     for i in s:
         current_Path = current_Path.replace(i, '/')
-        print(current_Path)
+        # print(current_Path)
 
     ffe_part = "/ffmpeg-master-latest-win64-gpl-shared/bin"
 
     join_part = current_Path + ffe_part
-    print(join_part)
+    # print(join_part)
 
     if re.findall(join_part, path):
         print("ffmpeg exit")
