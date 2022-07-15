b2c9a1038207f5a52b1b1c2e511ae9b4e87fffe1
Alan528 <83804199+Alan528@users.noreply.github.com>
2022-07-15 14:25:45 +0800

    fix environment install

--- test_ff.py
+++ test_ff.py
@@ -11,16 +11,32 @@ def test_ff():
     for i in s:
         path = path.replace(i, '/')
         print(path)
+    ffe_path = "C:/ffmpeg-master-latest-win64-gpl-shared/bin"
 
-    ffe_part = "C:/ffmpeg-master-latest-win64-gpl-shared/bin"
-
-    if re.findall(ffe_part, path):
-        print("ffmpeg exit")
-    else:
+    if not re.findall(ffe_path, path) and not os.path.exists("C:\\ffmpeg-master-latest-win64-gpl-shared\\bin"):
         print("ffmpeg not exit, Installing")
-        shutil.move(".\\ffmpeg-master-latest-win64-gpl-shared", "C:\\")
+        cmd = "xcopy /S /H /Y ffmpeg-master-latest-win64-gpl-shared c:\\ffmpeg-master-latest-win64-gpl-shared\\"
+        os.system(cmd)
         os.system("env.bat")
         print("please restart the program")
-        time.sleep(3)
+        time.sleep(5)
         exit()
+    elif not os.path.exists("C:\\ffmpeg-master-latest-win64-gpl-shared\\bin") and re.findall(ffe_path, path):
+        print("ffmpeg file not exit, Installing")
+        cmd = "xcopy /S /H /Y ffmpeg-master-latest-win64-gpl-shared c:\\ffmpeg-master-latest-win64-gpl-shared\\"
+        os.system(cmd)
+        print("please restart the program")
+        time.sleep(5)
+        exit()
+    elif not re.findall(ffe_path, path) and os.path.exists("C:\\ffmpeg-master-latest-win64-gpl-shared\\bin"):
+        print("ffmpeg environment not exit, Installing")
+        os.system("env.bat")
+        print("please restart the program")
+        time.sleep(5)
+        exit()
+    else:
+        print("ffmpeg exit")
 
+
+if __name__ == '__main__':
+    test_ff()
