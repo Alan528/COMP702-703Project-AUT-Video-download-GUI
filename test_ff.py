cdc59c5f0199b1db05ff01dcccc0dfe982969d6c
Alan528 <83804199+Alan528@users.noreply.github.com>
2022-07-19 22:03:38 +0800

    f

--- test_ff.py
+++ test_ff.py
@@ -1,5 +1,5 @@
 from libraries import *
-
+import sys
 
 def test_ff():
     s = ['\\']
@@ -16,6 +16,7 @@ def test_ff():
         os.system("admin.cmd")
         print("please restart the program")
         os.system("restartmes.cmd")
+        sys.exit()
 
     elif not os.path.exists("C:\\ffmpeg-master-latest-win64-gpl-shared\\bin") and re.findall(ffe_path, path):
         print("ffmpeg file not exit, Installing")
@@ -23,12 +24,14 @@ def test_ff():
         os.system(cmd)
         print("please restart the program")
         os.system("restartmes.cmd")
+        sys.exit()
 
     elif not re.findall(ffe_path, path) and os.path.exists("C:\\ffmpeg-master-latest-win64-gpl-shared\\bin"):
         print("ffmpeg environment not exit, Installing")
         os.system("admin.cmd")
         print("please restart the program")
         os.system("restartmes.cmd")
+        sys.exit()
 
     else:
         print("ffmpeg exit")
